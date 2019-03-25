e97621c34d3272430b9e7bde94775972768b4c9e
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2019-03-25 14:25:33 -0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,27 +1,46 @@
 from guizero import App, Text, PushButton
-import random
-list_a = []
-list_b = []
-list_c = []
+import os
+import time
+import threading
+import requests # *sigh* async guis are just too hard
 
-with open("/home/pi/python/shakespeare/insults.csv", "r") as f:
-    for line in f:
-        words = line.split(",")
-        list_a.append(words[0])
-        list_b.append(words[1])
-        list_c.append(words[2].strip())
-def insult():
-    a = random.choice(list_a)
-    b = random.choice(list_b)
-    c = random.choice(list_c)
-    insult = "Thou a " + a + " " + b + " " + c + "! \nGood Day! "
-    return insult
+server_url = 'http://192.168.1.65:8080'
+frequency = 1 # how often to set in seconds
+s = requests.Session()
+
+def get_cpu_temp():
+    """Returns the CPU temp in celsius. """
+    return os.popen("/opt/vc/bin/vcgencmd measure_temp").read().replace("=", " ")[5:-3]
+
+def get_gpu_temp():
+    """Returns the GPU temp in celsius. """
+    return "25"
+
+def main(mtext):
+    while True:
+        start = time.time()
+        cpu = get_cpu_temp()
+        gpu = get_gpu_temp()
+        params = {'cpu': cpu, 'gpu': gpu}
+        r = s.get(server_url + '/set', params=params)
+        mtext.value = r.text
+        taken = time.time() - start
+        print('Request took ' + str(taken))
+        if taken < frequency:
+            time.sleep(frequency - taken) # only sleeps for time
+            # still left before next request
+
+
 
 app = App(title="LCD Temperature", layout="auto", bg="white", width="750")
-mtext = Text(app, text="", size=20, color="black")
-def new_insult():
-    ins = insult()
-    mtext.value = ins
-btext = "Insult me again"
-button = PushButton(app, command=new_insult, text=btext)
+rtext = Text(app, text="", size=10, color="black")
+mtext = Text(app, text="", size=10, color="black")
+def reset_lcd():
+    r = s.get(server_url + '/reset')
+    rtext.value = 'Reset LCD, message was "' + r.text + '"'
+
+button = PushButton(app, command=reset_lcd, text="Reset LCD")
+thread = threading.Thread(target=main, args=(mtext,))
+thread.start()
 app.display()
+
